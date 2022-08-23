3b05bfc5560b52d27f157781a37e0e5a3740e5e2
Elecar1907 <carherelejos@gmail.com>
2022-08-23 04:59:12 -0500

    Fixbug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://elecar1907.github.io/GitHub-react-vite/',
   plugins: [react()]
 })
